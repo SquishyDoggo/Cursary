004d2f5cb8c06df80319c0386dcfb39678caaed1
SquishyDoggo <>
2021-08-09 08:15:52 +0200

    added more comments

--- EnJa.pas
+++ EnJa.pas
@@ -5,21 +5,23 @@ uses 	SysUtils, StrUtils;
 
 const 	DictName = 'DictEnJa.txt';
 	ProgramName = 'EnJa: The Terminal English-Japanese Vocabulary Trainer';
-	MaxSize = 2000; { max number of vocabulary in dictionary, a triplet is considered to be one vocabulary }
-	MultiTransSize = 5; { max number of english words, that point to same japanese word }
-	MultiTransDelim = ';'; { delimiter, if multiple english words are given }
+	MaxSize = 2000; 					{ max number of vocabulary in dictionary, a triplet is considered to be one vocabulary }
+	MultiTransSize = 5; 					{ max number of english words, that point to same japanese word }
+	MultiTransDelim = ';'; 					{ delimiter, if multiple english words are given }
 
-type 	TAllVoc = array [1..MaxSize,1..3] of String; { [x,y]: each x holds one vocabulary, i.e. english, kanji or hiragana and if still needed hiragana }
-						      { [x,y]: each y holds one specific part of the vocabulary, i.e. y=1: english, y=2: kanji, y=3: hiragana }
-	TMultiTrans = array [1..MultiTransSize] of String;
+type 	TAllVoc = array [1..MaxSize,1..3] of String; 		{ [x,y]: each x holds one vocabulary, i.e. english, kanji or hiragana and if still needed hiragana }
+						      		{ [x,y]: each y holds one specific part of the vocabulary, i.e. y=1: english, y=2: kanji, y=3: hiragana }
 
-	TVocRec = record
+	TMultiTrans = array [1..MultiTransSize] of String; 	{ contains multiple english translations if given }
+
+	TVocRec = record 					{ contains number of vocabulary and themselves }
 		AllVoc: TAllVoc;
 		VocNum: Integer;
 	end;
 
 var 	UserPromptDirection: Char;
 
+{ so that in operator can be used with type TMultiTrans }
 Operator in (A: String; B: TMultiTrans): Boolean;
 var 	i: Integer;
 begin
@@ -40,14 +42,14 @@ var 	MultiTrans: TMultiTrans;
 begin
 	i := 1;
 	ContainsMultiTrans := pos(MultiTransDelim,trans) > 0;
-	while pos(MultiTransDelim,trans) > 0 do 
+	while pos(MultiTransDelim,trans) > 0 do 		{ while there are multiple translations }
 	begin
 		MultiTrans[i] := copy(trans,1,pos(MultiTransDelim,trans)-1);
 		Delete(trans,1,pos(MultiTransDelim,trans));
 		Inc(i);
 	end;
-	if ContainsMultiTrans then MultiTrans[i] := trans
-	else MultiTrans[i] := '@!^'; { if there is only one translation, return garbage}
+	if ContainsMultiTrans then MultiTrans[i] := trans 	{ to get last translation as no ; exist after it and thus loop stops }
+	else MultiTrans[i] := '@!^'; 				{ if there is only one translation, return garbage }
 	result := MultiTrans;
 end;
 
@@ -63,6 +65,7 @@ begin
 	writeln(#10#13+ExpandedOrnament);
 end;
 
+{ create footer of program }
 procedure CreateFooter(Ornament: Char; OrnamentLength: Integer);
 var 	ExpandedOrnament: String;
 begin
@@ -78,58 +81,59 @@ var	Dict: TextFile;
 	i: Integer;
 	
 begin
-	Assign(Dict,DictName);
-	Reset(Dict);
+	Assign(Dict,DictName); Reset(Dict);
 	i:=1;
 	repeat
 		ReadLn(Dict,CurLine);
-		while CurLine = '' do ReadLn(Dict,CurLine);
-		result.AllVoc[i][1] := CurLine; { i.th voc, en}
+		while CurLine = '' do ReadLn(Dict,CurLine); 	{ read as long as empty lines are found }
+		result.AllVoc[i][1] := CurLine; 		{ i.th voc, en}
 		ReadLn(Dict,CurLine);
-		result.AllVoc[i][2] := CurLine; { i.th voc, ja}
+		result.AllVoc[i][2] := CurLine; 		{ i.th voc, ja}
 		ReadLn(Dict,CurLine);
-		result.AllVoc[i][3] := CurLine; { i.th voc, hira if present, else empty}
+		result.AllVoc[i][3] := CurLine; 		{ i.th voc, hira if present, else empty}
 		Inc(i);
 	until eof(Dict);
-	result.VocNum := i-1; 			{ number of vocabulary stored }
+	result.VocNum := i-1; 					{ number of vocabulary stored }
 	close(Dict);
 end;
 
 { prompt user for english -> japanese translations }
 procedure PromptVocEnJa(DictName: String; VocIndex: Integer);
-var 	UserTrans: String;
+var 	UserTrans, En, Ja, Hira: String;
 	VocRec: TVocRec;
 begin
 	VocRec := GetNVoc(DictName);	
-	write(#10#13+VocRec.AllVoc[VocIndex][1]+': ');
+	En := VocRec.AllVoc[VocIndex][1]; Ja := VocRec.AllVoc[VocIndex][2]; Hira := VocRec.AllVoc[VocIndex][3];
+	write(#10#13+En+': ');
 	readln(UserTrans);
-	if (UserTrans = '') and (VocRec.AllVoc[VocIndex][3] <> '') then writeln('Incorrect. Correct answer: '+VocRec.AllVoc[VocIndex][2]+' ['+VocRec.AllVoc[VocIndex][3]+']')
-	else if (UserTrans = '') and (VocRec.AllVoc[VocIndex][3] = '') then writeln('Incorrect. Correct answer: '+VocRec.AllVoc[VocIndex][2])
+	if (UserTrans = '') and (Hira <> '') then writeln('Incorrect. Correct answer: '+Ja+' ['+Hira+']')
+	else if (UserTrans = '') and (Hira = '') then writeln('Incorrect. Correct answer: '+Ja)
 	else
-		if (CompareText(UserTrans,VocRec.AllVoc[VocIndex][2]) <> 0) and (CompareText(UserTrans,VocRec.AllVoc[VocIndex][3]) <> 0) then 
-			if VocRec.AllVoc[VocIndex][3] <> '' then writeln('Incorrect. Correct answer: '+VocRec.AllVoc[VocIndex][2]+' ['+VocRec.AllVoc[VocIndex][3]+']')
-			else writeln('Incorrect. Correct answer: '+VocRec.AllVoc[VocIndex][2])
-		else if CompareText(UserTrans,VocRec.AllVoc[VocIndex][2]) = 0 then writeln('Correct.')
-		else if (CompareText(UserTrans,VocRec.AllVoc[VocIndex][3]) = 0) and (VocRec.AllVoc[VocIndex][3] <> '') then
-			writeln('Correct. Kanji notation exists: '+VocRec.AllVoc[VocIndex][2]+' ['+VocRec.AllVoc[VocIndex][3]+']')
+		if (CompareText(UserTrans,Ja) <> 0) and (CompareText(UserTrans,Hira) <> 0) then 
+			if Hira <> '' then writeln('Incorrect. Correct answer: '+Ja+' ['+Hira+']')
+			else writeln('Incorrect. Correct answer: '+Ja)
+		else if CompareText(UserTrans,Ja) = 0 then writeln('Correct.')
+		else if (CompareText(UserTrans,Hira) = 0) and (Hira <> '') then
+			writeln('Correct. Kanji notation exists: '+Ja+' ['+Hira+']')
 		else 	writeln('A wild unhandled statement appeard. Catch your current vocabulary and report it!');
 end;
 
 { prompt user for japanese -> english translations }
 { need to add verification if more than one and less than all UserTrans are given, currently message is always 'Incorrect.' }
 procedure PromptVocJaEn(DictName: String; VocIndex: Integer);
-var 	UserTrans: String;
+var 	UserTrans, En, Ja, Hira: String;
 	VocRec: TVocRec;
 	MultiTrans: TMultiTrans;
 begin
 	VocRec := GetNVoc(DictName);	
-	MultiTrans := GetMultiTrans(VocRec.AllVoc[VocIndex][1]);
-	if VocRec.AllVoc[VocIndex][3] <> '' then write(#10#13+VocRec.AllVoc[VocIndex][2]+' ['+VocRec.AllVoc[VocIndex][3]+']'+': ')
-	else write(#10#13+VocRec.AllVoc[VocIndex][2]+': '); 
+	En := VocRec.AllVoc[VocIndex][1]; Ja := VocRec.AllVoc[VocIndex][2]; Hira := VocRec.AllVoc[VocIndex][3];
+	MultiTrans := GetMultiTrans(En);
+	if Hira <> '' then write(#10#13+Ja+' ['+Hira+']'+': ')
+	else write(#10#13+Ja+': '); 
 	
 	readln(UserTrans);
-	if (UserTrans = '') or ( not (UserTrans in MultiTrans) ) and ( CompareText(VocRec.AllVoc[VocIndex][1],UserTrans) <> 0 ) then writeln('Incorrect. Correct answer: '+VocRec.AllVoc[VocIndex][1])
-	else if (UserTrans in MultiTrans) then writeln('Correct. All solutions: '+VocRec.AllVoc[VocIndex][1])
+	if (UserTrans = '') or ( not (UserTrans in MultiTrans) ) and ( CompareText(En,UserTrans) <> 0 ) then writeln('Incorrect. Correct answer: '+En)
+	else if (UserTrans in MultiTrans) then writeln('Correct. All solutions: '+En)
 	else writeln('Correct.');
 end;
 
